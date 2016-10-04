cd9e56a366270052baa7082e766d742d2aa122dd
cmeyer90 <cmeyer90@users.noreply.github.com>
2016-10-04 12:47:05 -0500

    Update user.js

--- user.js
+++ user.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		console.log("1");
 		$.ajax({
 			type: 'POST',
-			url: 'http://api.hackerearth.com/code/compile/',
+			url: '/post.php',
 			data: {
 				client_secret:'****',    //add your client secret here for testing
 				lang: $("#lang").val(),
