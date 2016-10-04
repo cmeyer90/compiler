ea8cfaaeff13e974920c7c8aea5e79bf9ec6edc2
cmeyer90 <cmeyer90@users.noreply.github.com>
2016-10-04 12:46:23 -0500

    Create post.php

---
+++ post.php
@@ -0,0 +1,15 @@
+<?php
+
+//set up post data
+$post_data = 'client_secret=' . $_POST['client_secret'] . '&lang=' . $_POST['lang'] . '&source=' . urlencode($_POST['source']);
+
+//initiate CURL request
+$ch = curl_init('http://api.hackerearth.com/code/compile/');                                                                      
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);                                                                  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                                                                                        
+
+//echo result for AJAX call
+$result = curl_exec($ch);
+echo $result;
+?>
